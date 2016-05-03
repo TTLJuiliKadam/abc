45f5a4b1adddbd941a618db2a1a14ed61774f20d
Juili <juili@sk.local>
2016-05-03 11:34:57 +0530

    Initial Commit

---
+++ abc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  abc
+//
+//  Created by Juili on 03/05/16.
+//  Copyright © 2016 Juili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ abc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  abc
+//
+//  Created by Juili on 03/05/16.
+//  Copyright © 2016 Juili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
