175ef379c22f245defe9682485692fa79dd0d3dc
Juili <juili@sk.local>
2016-05-03 11:51:59 +0530

    new

---
+++ abc/new.h
@@ -0,0 +1,13 @@
+//
+//  new.h
+//  abc
+//
+//  Created by Juili on 03/05/16.
+//  Copyright © 2016 Juili. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface new : NSObject
+
+@end
---
+++ abc/new.m
@@ -0,0 +1,13 @@
+//
+//  new.m
+//  abc
+//
+//  Created by Juili on 03/05/16.
+//  Copyright © 2016 Juili. All rights reserved.
+//
+
+#import "new.h"
+
+@implementation new
+
+@end
